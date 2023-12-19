83d73ffb6fe2c867f482bcb9f64e853bde8d200c
minseok <alstjr706@naver.com>
2023-12-19 21:54:12 +0900

    git : SignupBranch 생성

---
+++ src/main/java/com/example/github/SignupBranch.java
@@ -0,0 +1,10 @@
+package com.example.github;
+
+public class SignupBranch {
+
+    private void feature() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("그냥 작성");
+        }
+    }
+}
