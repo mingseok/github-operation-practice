effcccad42eb4678fc5181be001bcd42fa9e8fa4
minseok <alstjr706@naver.com>
2024-02-26 23:28:49 +0900

    test

--- src/main/java/com/example/github/GithubApplication.java
+++ src/main/java/com/example/github/GithubApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubApplication {
 
-	// git test 24.02.10
-
 	public static void main(String[] args) {
 		SpringApplication.run(GithubApplication.class, args);
 	}
---
+++ src/main/java/com/example/github/test/A.java
@@ -0,0 +1,7 @@
+package com.example.github.test;
+
+public class A {
+
+    private int id;
+    private String name;
+}
