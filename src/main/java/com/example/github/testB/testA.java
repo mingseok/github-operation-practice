a3e19792b5f5048833881c765bf5f8cd9ed85be4
minseok <alstjr706@naver.com>
2024-03-11 22:32:44 +0900

    merge: develop test 03.11

--- src/main/java/com/example/github/testB/testA.java
+++ src/main/java/com/example/github/testB/testA.java
@@ -2,7 +2,5 @@ package com.example.github.testB;
 
 public class testA {
 
-    // Branch B 테스트
-
-    // B 브랜치 추가 변경 사항
+    // 03.11 test
 }
--- src/main/java/com/example/github/testC/add.java
+++ src/main/java/com/example/github/testC/add.java
@@ -2,12 +2,5 @@ package com.example.github.testC;
 
 public class add {
 
-    // 03.01 test
-
-    // 03.02 수정
-
-    // 03.02 test
-    private int id;
-    private String name;
-
+    // 03.11
 }
