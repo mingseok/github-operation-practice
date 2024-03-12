96c90c71858873f3b7c96aa86614ee4381041789
Minseok Kim <alstjr706@naver.com>
2024-03-12 20:49:01 +0900

    Update A.java

--- src/main/java/com/example/github/test/A.java
+++ src/main/java/com/example/github/test/A.java
@@ -2,13 +2,5 @@ package com.example.github.test;
 
 public class A {
 
-    private int id;
-    private String name;
-
-    private int a;
-    private String b;
-
-    // B 브랜치 추가 변경 사항
-
-    // 03.02 수정
+    // 03.12 수정
 }
