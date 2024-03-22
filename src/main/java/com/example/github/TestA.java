f62b55a4fbbef2993d6149997373476a0831dc5b
Minseok Kim <alstjr706@naver.com>
2024-03-22 22:48:57 +0900

    merge: test 03.22

--- src/main/java/com/example/github/TestA.java
+++ src/main/java/com/example/github/TestA.java
@@ -1,5 +1,6 @@
 package com.example.github;
 
 public class TestA {
-    // test 03.22
+    // test 03.22 - B
+    // test 03.22 - B
 }
---
+++ src/main/java/com/example/github/TestB.java
@@ -0,0 +1,5 @@
+package com.example.github;
+
+public class TestB {
+    // test 03.22
+}
