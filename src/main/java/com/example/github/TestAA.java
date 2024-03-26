15554d01268bb6d9256990594542f4526dab4f95
Minseok Kim <alstjr706@naver.com>
2024-03-26 21:41:11 +0900

    merge: test 03.26 - 2

--- src/main/java/com/example/github/TestAA.java
+++ src/main/java/com/example/github/TestAA.java
@@ -1,7 +1,5 @@
 package com.example.github;
 
 public class TestAA {
-    // test 03.25 수정
-
-    // test 03.26 수정
+    // test 03.26 수정 - 2
 }
--- src/main/java/com/example/github/TestBB.java
+++ src/main/java/com/example/github/TestBB.java
@@ -1,5 +1,5 @@
 package com.example.github;
 
 public class TestBB {
-    // test 03.26 업데이트
+    // test 03.26 업데이트 - 2
 }
