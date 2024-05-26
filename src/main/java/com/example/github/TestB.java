5b13d7c622f0ff067c1d29d6fb9761a30a4b7ca1
Minseok Kim <alstjr706@naver.com>
2024-05-26 11:10:31 +0900

    Rebase test

--- src/main/java/com/example/github/TestB.java
+++ src/main/java/com/example/github/TestB.java
@@ -3,4 +3,6 @@ package com.example.github;
 public class TestB {
 
     // test
+
+    // 리베이스 테스트
 }
--- src/main/java/com/example/github/TestBB.java
+++ src/main/java/com/example/github/TestBB.java
@@ -3,4 +3,6 @@ package com.example.github;
 public class TestBB {
 
     // test
+
+    // 05.26
 }
