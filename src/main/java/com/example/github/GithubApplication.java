3a23f92adf633ef455e5395f72b47da293b35f3b
Minseok Kim <alstjr706@naver.com>
2024-08-23 21:45:24 +0900

    git test

--- src/main/java/com/example/github/GithubApplication.java
+++ src/main/java/com/example/github/GithubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubApplication {
 
 	public static void main(String[] args) {
+		// git test
 		SpringApplication.run(GithubApplication.class, args);
 	}
 
